b575aa1e00db7e236773bdc099119f92a5278465
aquashie14 <113648017+aquashie14@users.noreply.github.com>
2022-10-26 18:57:25 +0000

    Update forces.py

--- forces.py
+++ forces.py
@@ -1,7 +1,7 @@
 #This code will helps us compare two forces given some parameters
 # m and M are mass of the body and earth respectively
 # R and r are the radii of the body and the earth respectively
-# v is the velocity of th body
+# v is the velocity of the body
 
 m = float(input("Enter the mass of the body: "))
 r = float(input("Enter the radius of the body: "))
@@ -15,7 +15,7 @@ R = 6.37 *(10**6)
 C_force = (m * (v**2)) / r
 G_force = (G * m * M) / (R**2)
 
-# compare the teo forces 
+# compare the the forces 
 if (C_force == G_force):
     print ("The two forces are equal")
 else:
